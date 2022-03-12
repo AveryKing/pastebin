3c8fa6287a9d7d89113802374664411d3fcf9d74
Avery King <avery.n.king@wmich.edu>
2022-03-12 17:49:52 -0500

    error handling added for post method

--- src/app.js
+++ src/app.js
@@ -19,16 +19,20 @@ let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 app.post("/pastes", (req, res, next) => {
 
   const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+  if(text) {
+    const newPaste = {
+      id: ++lastPasteId, // Increment last ID, then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+      user_id,
+    };
+  } else {
+    res.sendStatus(400);
+  }
 
-  const newPaste = {
-    id: ++lastPasteId, // Increment last ID, then assign as the current ID
-    name,
-    syntax,
-    exposure,
-    expiration,
-    text,
-    user_id,
-  };
 
   pastes.push(newPaste);
 
