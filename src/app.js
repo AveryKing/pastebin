451ffb164f4ec99bf9bfa55a5df5e95aa5e1138b
Avery King <avery.n.king@wmich.edu>
2022-03-12 17:46:51 -0500

    post route added

--- src/app.js
+++ src/app.js
@@ -10,6 +10,31 @@ app.get('/pastes', (req,res) => {
   res.json({data:pastes});
 })
 
+// Variable to hold the next ID
+
+// Because some IDs may already be used, find the largest assigned ID
+
+let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
+
+app.post("/pastes", (req, res, next) => {
+
+  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
+
+  const newPaste = {
+    id: ++lastPasteId, // Increment last ID, then assign as the current ID
+    name,
+    syntax,
+    exposure,
+    expiration,
+    text,
+    user_id,
+  };
+
+  pastes.push(newPaste);
+
+  res.json({ data: newPaste });
+
+});
 app.get('/pastes/:pasteId', (req,res, next) => {
   const foundPaste = pastes.find(x => x.id === Number(req.params.pasteId));
 
