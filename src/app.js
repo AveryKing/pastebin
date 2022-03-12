aafb44d0b89538b679f1cd030c0a69863bcc98b9
Avery King <avery.n.king@wmich.edu>
2022-03-12 17:55:13 -0500

    request body validation extracted to middleware

--- src/app.js
+++ src/app.js
@@ -6,9 +6,26 @@ app.use(express.json());
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
 
-app.get('/pastes', (req,res) => {
-  res.json({data:pastes});
+app.get('/pastes', (req, res) => {
+    res.json({data: pastes});
 })
+
+// New middleware function to validate the request body
+
+function bodyHasTextProperty(req, res, next) {
+
+    const {data: {text} = {}} = req.body;
+
+    if (text) {
+
+        return next(); // Call `next()` without an error message if the result exists
+
+    }
+
+    next("A 'text' property is required.");
+
+}
+
 
 // Variable to hold the next ID
 
@@ -16,49 +33,47 @@ app.get('/pastes', (req,res) => {
 
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.post("/pastes", (req, res, next) => {
+app.post("/pastes",
+    bodyHasTextProperty,
+    (req, res, next) => {
+        const {data: {name, syntax, exposure, expiration, text, user_id} = {}} = req.body;
+        const newPaste = {
+            id: ++lastPasteId, // Increment last ID, then assign as the current ID
+            name,
+            syntax,
+            exposure,
+            expiration,
+            text,
+            user_id,
+        };
+            
+        pastes.push(newPaste);
 
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-  if(text) {
-    const newPaste = {
-      id: ++lastPasteId, // Increment last ID, then assign as the current ID
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    };
-  } else {
-    res.sendStatus(400);
-  }
+        res.status(201).json({data: newPaste});
+
+    });
 
 
-  pastes.push(newPaste);
+app.get('/pastes/:pasteId', (req, res, next) => {
+    const foundPaste = pastes.find(x => x.id === Number(req.params.pasteId));
 
-  res.status(201).json({ data: newPaste });
-
-});
-app.get('/pastes/:pasteId', (req,res, next) => {
-  const foundPaste = pastes.find(x => x.id === Number(req.params.pasteId));
-
-  if(foundPaste) {
-    return res.json(foundPaste)
-  } else {
-    next("paste not found")
-  }
+    if (foundPaste) {
+        return res.json(foundPaste)
+    } else {
+        next("paste not found")
+    }
 
 })
 
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+    next(`Not found: ${request.originalUrl}`);
 });
 
 // Error handler
 app.use((error, request, response, next) => {
-  console.error(error);
-  response.send(error);
+    console.error(error);
+    response.send(error);
 });
 
 module.exports = app;
