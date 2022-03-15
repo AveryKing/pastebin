318aa2a840c89967a627f1f5c05ad9e243d57d65
Avery King <avery.n.king@wmich.edu>
2022-03-14 21:41:16 -0400

    post creation extracted to controller

--- src/app.js
+++ src/app.js
@@ -8,45 +8,6 @@ app.use(express.json());
 
 app.use("/pastes", pastesRouter);
 
-// New middleware function to validate the request body
-
-function bodyHasTextProperty(req, res, next) {
-    const {data: {text} = {}} = req.body;
-    if (text) {
-        return next(); // Call `next()` without an error message if the result exists
-    }
-    next({
-        status:400,
-        message:"A 'text' property is required."
-    });
-}
-
-
-// Variable to hold the next ID
-
-// Because some IDs may already be used, find the largest assigned ID
-
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-
-app.post("/pastes",
-    bodyHasTextProperty,
-    (req, res, next) => {
-        const {data: {name, syntax, exposure, expiration, text, user_id} = {}} = req.body;
-        const newPaste = {
-            id: ++lastPasteId, // Increment last ID, then assign as the current ID
-            name,
-            syntax,
-            exposure,
-            expiration,
-            text,
-            user_id,
-        };
-
-        pastes.push(newPaste);
-        res.status(201).json({data: newPaste});
-
-    });
-
 
 app.get('/pastes/:pasteId', (req, res, next) => {
     const foundPaste = pastes.find(x => x.id === Number(req.params.pasteId));
